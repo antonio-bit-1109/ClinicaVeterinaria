13acf9988935fb864c1a4c9319a535df83ed695f
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-03-19 11:13:59 +0100

    ggg

--- ClinicaVeterinaria/Controllers/AnimaliController.cs
+++ ClinicaVeterinaria/Controllers/AnimaliController.cs
@@ -5,174 +5,177 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ClinicaVeterinaria.Controllers
 {
-	public class AnimaliController : Controller
-	{
-		private readonly SocityPetContext _context;
+    public class AnimaliController : Controller
+    {
+        private readonly SocityPetContext _context;
 
-		public AnimaliController(SocityPetContext context)
-		{
-			_context = context;
-		}
+        public AnimaliController(SocityPetContext context)
+        {
+            _context = context;
+        }
 
-		// GET: Animali
-		public async Task<IActionResult> Index()
-		{
+        // GET: Animali
+        public async Task<IActionResult> Index()
+        {
 
-			var socityPetContext = _context.Animalis.Include(a => a.IdUtenteNavigation);
-			return View(await socityPetContext.ToListAsync());
-		}
+            var socityPetContext = _context.Animalis.Include(a => a.IdUtenteNavigation);
+            return View(await socityPetContext.ToListAsync());
+        }
 
-		// GET: Animali/Details/5
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        // GET: Animali/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-			var animali = await _context.Animalis
-				.Include(a => a.IdUtenteNavigation)
-				.FirstOrDefaultAsync(m => m.Idanimale == id);
-			if (animali == null)
-			{
-				return NotFound();
-			}
+            var animali = await _context.Animalis
+                .Include(a => a.IdUtenteNavigation)
+                .FirstOrDefaultAsync(m => m.Idanimale == id);
+            if (animali == null)
+            {
+                return NotFound();
+            }
 
-			return View(animali);
-		}
+            return View(animali);
+        }
 
-		// GET: Animali/Create
-		[HttpGet]
-		public IActionResult Create()
-		{
-			ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "Nome");
-			return View();
-		}
+        // GET: Animali/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
 
-		// POST: Animali/Create
+            ViewData["IdUtente"] = new SelectList(_context.Utentis.Where(u => u.IdRuolo != 4).Select(u => new { u.IdUtente, NomeCompleto = u.Nome + " " + u.Cognome }), "IdUtente", "NomeCompleto");
+            return View();
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("Dataregistrazione,NomeAnimale,Tipologia,ColoreMantello," +
-			"Datanascita,HasMicrochip,NumMicrochip,HasProprietario,IdUtente")] Animali animali, IFormFile FotoAnimale)
-		{
-			ModelState.Remove("IdUtenteNavigation");
-			ModelState.Remove("Ricoveris");
-			ModelState.Remove("Visites");
+        // POST: Animali/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Dataregistrazione,NomeAnimale,Tipologia,ColoreMantello," +
+            "Datanascita,HasMicrochip,NumMicrochip,HasProprietario,IdUtente")] Animali animali, IFormFile FotoAnimale)
+        {
+            ModelState.Remove("IdUtenteNavigation");
+            ModelState.Remove("Ricoveris");
+            ModelState.Remove("Visites");
 
 
-			if (ModelState.IsValid)
-			{
-				if (FotoAnimale != null && FotoAnimale.Length > 0)
-				{
-					var fileName = Path.GetFileName(FotoAnimale.FileName);
-					var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/animali", fileName);
+            if (ModelState.IsValid)
+            {
+                if (FotoAnimale != null && FotoAnimale.Length > 0)
+                {
+                    var fileName = Path.GetFileName(FotoAnimale.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/animali", fileName);
 
-					using (var fileStream = new FileStream(filePath, FileMode.Create))
-					{
-						await FotoAnimale.CopyToAsync(fileStream);
-					}
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await FotoAnimale.CopyToAsync(fileStream);
+                    }
 
-					animali.FotoAnimale = fileName;
-				}
+                    animali.FotoAnimale = fileName;
+                }
 
-				_context.Add(animali);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "Nome", animali.IdUtente);
-			return RedirectToAction("Index");
-		}
+                _context.Add(animali);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "Nome", animali.IdUtente);
+            return RedirectToAction("Index");
+        }
 
-		// GET: Animali/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        // GET: Animali/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-			var animali = await _context.Animalis.FindAsync(id);
-			if (animali == null)
-			{
-				return NotFound();
-			}
-			ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "Nome", animali.IdUtente);
-			return View(animali);
-		}
+            var animali = await _context.Animalis.FindAsync(id);
+            if (animali == null)
+            {
+                return NotFound();
+            }
+            //ViewData["FotoAnimale"] = animali.FotoAnimale;
+            ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "Nome", animali.IdUtente);
+            return View(animali);
+        }
 
-		// POST: Animali/Edit/5
+        // POST: Animali/Edit/5
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("Idanimale,Dataregistrazione,NomeAnimale,Tipologia,ColoreMantello,Datanascita,HasMicrochip,NumMicrochip,FotoAnimale,HasProprietario,IdUtente")] Animali animali)
-		{
-			if (id != animali.Idanimale)
-			{
-				return NotFound();
-			}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idanimale,Dataregistrazione,NomeAnimale,Tipologia," +
+            "ColoreMantello,Datanascita,HasMicrochip,NumMicrochip,HasProprietario,IdUtente")] Animali animali, IFormFile FotoAnimale)
+        {
+            if (id != animali.Idanimale)
+            {
+                return NotFound();
+            }
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(animali);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!AnimaliExists(animali.Idanimale))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "IdUtente", animali.IdUtente);
-			return View(animali);
-		}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animali);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimaliExists(animali.Idanimale))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUtente"] = new SelectList(_context.Utentis, "IdUtente", "IdUtente", animali.IdUtente);
+            return View(animali);
+        }
 
-		// GET: Animali/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        // GET: Animali/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-			var animali = await _context.Animalis
-				.Include(a => a.IdUtenteNavigation)
-				.FirstOrDefaultAsync(m => m.Idanimale == id);
-			if (animali == null)
-			{
-				return NotFound();
-			}
+            var animali = await _context.Animalis
+                .Include(a => a.IdUtenteNavigation)
+                .FirstOrDefaultAsync(m => m.Idanimale == id);
+            if (animali == null)
+            {
+                return NotFound();
+            }
 
-			return View(animali);
-		}
+            return View(animali);
+        }
 
-		// POST: Animali/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			var animali = await _context.Animalis.FindAsync(id);
-			if (animali != null)
-			{
-				_context.Animalis.Remove(animali);
-			}
+        // POST: Animali/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var animali = await _context.Animalis.FindAsync(id);
+            if (animali != null)
+            {
+                _context.Animalis.Remove(animali);
+            }
 
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
-		private bool AnimaliExists(int id)
-		{
-			return _context.Animalis.Any(e => e.Idanimale == id);
-		}
-	}
+        private bool AnimaliExists(int id)
+        {
+            return _context.Animalis.Any(e => e.Idanimale == id);
+        }
+    }
 }
---
+++ ClinicaVeterinaria/wwwroot/js/createAnimale_show_hide.js
@@ -0,0 +1,26 @@
+﻿const inputHasMicrochip = document.getElementById("inputHasMicrochip");
+const divNumMicrochip = document.getElementById("divNumMicrochip");
+const inputHasProprietario = document.getElementById("inputHasProprietario");
+const divIdUtente = document.getElementById("divIdUtente");
+
+const mostraNascondi = (input, div) => {
+    if (!input.checked) {
+        div.style.display = "none";
+    }
+
+    input.addEventListener('change', () => {
+        if (!input.checked) {
+            div.style.display = "none";
+        } else {
+            div.style.display = "block";
+        }
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    mostraNascondi(inputHasMicrochip, divNumMicrochip);
+    mostraNascondi(inputHasProprietario, divIdUtente);
+
+});
