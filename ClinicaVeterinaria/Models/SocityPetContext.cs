6af0d212d4a79f8a13835bc0d12228fb1b4e8518
mattia lugaro <mattialugaro@gmail.com>
2024-03-22 12:05:00 +0100

    correzione home e login

--- ClinicaVeterinaria/Models/SocityPetContext.cs
+++ ClinicaVeterinaria/Models/SocityPetContext.cs
@@ -41,7 +41,7 @@ public partial class SocityPetContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-QEIG4Q2\\SQLEXPRESS;Database=SocityPet;TrustServerCertificate=true;Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-0AIEBQTF\\SQLEXPRESS;Database=SocityPet;TrustServerCertificate=true;Trusted_Connection=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
