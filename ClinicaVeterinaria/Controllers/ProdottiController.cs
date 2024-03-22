72de6361fca84364318d0d85a027303daa8d4cea
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-03-22 11:09:30 +0100

    giggio

--- ClinicaVeterinaria/Controllers/ProdottiController.cs
+++ ClinicaVeterinaria/Controllers/ProdottiController.cs
@@ -2,19 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace ClinicaVeterinaria.Controllers
 {
-    public class ProdottiController : Controller
-    {
-        private readonly SocityPetContext _context;
+	public class ProdottiController : Controller
+	{
+		private readonly SocityPetContext _context;
 
-        public ProdottiController(SocityPetContext context)
-        {
-            _context = context;
-        }
+		public ProdottiController(SocityPetContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Prodotti
+		// GET: Prodotti
 
 
         public async Task<IActionResult> Index(string prodotto)
@@ -67,198 +68,281 @@ namespace ClinicaVeterinaria.Controllers
         //                            < ========================================================
 
 
-        // GET: Prodotti/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            string navfoot = "farm";
-            ViewBag.NavFoot = navfoot;
-            string text = "wh";
-            ViewBag.Text = text;
+		// GET: Prodotti/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			string navfoot = "farm";
+			ViewBag.NavFoot = navfoot;
+			string text = "wh";
+			ViewBag.Text = text;
 
-            if (id == null)
-            {
-                return NotFound();
-            }
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var prodotti = await _context.Prodottis
-                .Include(p => p.IdDittaFornitriceNavigation)
-                .FirstOrDefaultAsync(m => m.IdProdotto == id);
+			var prodotti = await _context.Prodottis
+				.Include(p => p.IdDittaFornitriceNavigation)
+				.FirstOrDefaultAsync(m => m.IdProdotto == id);
 
-            if (prodotti == null)
+			if (prodotti == null)
 
-            {
-                return NotFound();
-            }
+			{
+				return NotFound();
+			}
 
-            return View(prodotti);
-        }
+			return View(prodotti);
+		}
 
-        // GET: Prodotti/Create
+		// GET: Prodotti/Create
 
-        public IActionResult Create()
-        {
-            string navfoot = "farm";
-            ViewBag.NavFoot = navfoot;
-            string text = "wh";
-            ViewBag.Text = text;
+		public IActionResult Create()
+		{
+			string navfoot = "farm";
+			ViewBag.NavFoot = navfoot;
+			string text = "wh";
+			ViewBag.Text = text;
 
-            ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice");
-            return View();
-        }
+			ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice");
+			return View();
+		}
 
-        // POST: Prodotti/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		// POST: Prodotti/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 
 
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Nomeprodotto,Prezzo,IdDittaFornitrice,IsMedicinale,PossibiliUsi")] Prodotti prodotti, IFormFile uploadedImage)
-        {
-            ModelState.Remove("IdDittaFornitriceNavigation");
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Nomeprodotto,Prezzo,IdDittaFornitrice,IsMedicinale,PossibiliUsi")] Prodotti prodotti, IFormFile uploadedImage)
+		{
+			ModelState.Remove("IdDittaFornitriceNavigation");
 
-            if (ModelState.IsValid)
-            {
-                if (uploadedImage != null && uploadedImage.Length > 0)
-                {
-                    var fileName = Path.GetFileName(uploadedImage.FileName);
-                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/prodotti", fileName);
+			if (ModelState.IsValid)
+			{
+				if (uploadedImage != null && uploadedImage.Length > 0)
+				{
+					var fileName = Path.GetFileName(uploadedImage.FileName);
+					var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/prodotti", fileName);
 
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await uploadedImage.CopyToAsync(fileStream);
-                    }
+					using (var fileStream = new FileStream(filePath, FileMode.Create))
+					{
+						await uploadedImage.CopyToAsync(fileStream);
+					}
 
-                    prodotti.FotoProdotto = fileName;
-                }
+					prodotti.FotoProdotto = fileName;
+				}
 
-                _context.Add(prodotti);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+				_context.Add(prodotti);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
 
-            }
+			}
 
-            ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice", prodotti.IdDittaFornitrice);
-            return View(prodotti);
-        }
+			ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice", prodotti.IdDittaFornitrice);
+			return View(prodotti);
+		}
 
-        // GET: Prodotti/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            string navfoot = "farm";
-            ViewBag.NavFoot = navfoot;
-            string text = "wh";
-            ViewBag.Text = text;
+		// GET: Prodotti/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			string navfoot = "farm";
+			ViewBag.NavFoot = navfoot;
+			string text = "wh";
+			ViewBag.Text = text;
 
-            if (id == null)
-            {
-                return NotFound();
-            }
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var prodotti = await _context.Prodottis.FindAsync(id);
-            if (prodotti == null)
-            {
-                return NotFound();
-            }
-            ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice", prodotti.IdDittaFornitrice);
-            return View(prodotti);
-        }
+			var prodotti = await _context.Prodottis.FindAsync(id);
+			if (prodotti == null)
+			{
+				return NotFound();
+			}
+			ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice", prodotti.IdDittaFornitrice);
+			return View(prodotti);
+		}
 
-        // POST: Prodotti/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdProdotto,Nomeprodotto,Prezzo,IdDittaFornitrice,IsMedicinale,PossibiliUsi")] Prodotti prodotti, IFormFile uploadedImage)
-        {
-            ModelState.Remove("IdDittaFornitriceNavigation");
-            ModelState.Remove("uploadedImage");
+		// POST: Prodotti/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("IdProdotto,Nomeprodotto,Prezzo,IdDittaFornitrice,IsMedicinale,PossibiliUsi")] Prodotti prodotti, IFormFile uploadedImage)
+		{
+			ModelState.Remove("IdDittaFornitriceNavigation");
+			ModelState.Remove("uploadedImage");
 
-            if (id != prodotti.IdProdotto)
-            {
-                return NotFound();
-            }
+			if (id != prodotti.IdProdotto)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                if (uploadedImage != null && uploadedImage.Length > 0)
-                {
-                    var fileName = Path.GetFileName(uploadedImage.FileName);
-                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/prodotti", fileName);
+			if (ModelState.IsValid)
+			{
+				if (uploadedImage != null && uploadedImage.Length > 0)
+				{
+					var fileName = Path.GetFileName(uploadedImage.FileName);
+					var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/prodotti", fileName);
 
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await uploadedImage.CopyToAsync(fileStream);
-                    }
+					using (var fileStream = new FileStream(filePath, FileMode.Create))
+					{
+						await uploadedImage.CopyToAsync(fileStream);
+					}
 
-                    prodotti.FotoProdotto = fileName;
-                }
+					prodotti.FotoProdotto = fileName;
+				}
 
-                try
-                {
-                    _context.Update(prodotti);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProdottiExists(prodotti.IdProdotto))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice", prodotti.IdDittaFornitrice);
-            return View(prodotti);
-        }
+				try
+				{
+					_context.Update(prodotti);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ProdottiExists(prodotti.IdProdotto))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["IdDittaFornitrice"] = new SelectList(_context.Dittafornitrices, "IdDittaFornitrice", "IdDittaFornitrice", prodotti.IdDittaFornitrice);
+			return View(prodotti);
+		}
 
-        // GET: Prodotti/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            string navfoot = "farm";
-            ViewBag.NavFoot = navfoot;
-            string text = "wh";
-            ViewBag.Text = text;
+		// GET: Prodotti/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			string navfoot = "farm";
+			ViewBag.NavFoot = navfoot;
+			string text = "wh";
+			ViewBag.Text = text;
 
-            if (id == null)
-            {
-                return NotFound();
-            }
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var prodotti = await _context.Prodottis
-                .Include(p => p.IdDittaFornitriceNavigation)
-                .FirstOrDefaultAsync(m => m.IdProdotto == id);
-            if (prodotti == null)
-            {
-                return NotFound();
-            }
+			var prodotti = await _context.Prodottis
+				.Include(p => p.IdDittaFornitriceNavigation)
+				.FirstOrDefaultAsync(m => m.IdProdotto == id);
+			if (prodotti == null)
+			{
+				return NotFound();
+			}
 
-            return View(prodotti);
-        }
+			return View(prodotti);
+		}
 
-        // POST: Prodotti/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var prodotti = await _context.Prodottis.FindAsync(id);
-            if (prodotti != null)
-            {
-                _context.Prodottis.Remove(prodotti);
-            }
+		// POST: Prodotti/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var prodotti = await _context.Prodottis.FindAsync(id);
+			if (prodotti != null)
+			{
+				_context.Prodottis.Remove(prodotti);
+			}
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool ProdottiExists(int id)
-        {
-            return _context.Prodottis.Any(e => e.IdProdotto == id);
-        }
-    }
+		private bool ProdottiExists(int id)
+		{
+			return _context.Prodottis.Any(e => e.IdProdotto == id);
+		}
+
+
+		public async Task<IActionResult> AggiungiAlCarrello(int? idProdotto)
+		{
+			bool prodottoGiaPresenteNelCarrello = false;
+			var prodottoSelezionato = _context.Prodottis.Find(idProdotto);
+
+			// se il prodotto esiste
+			if (prodottoSelezionato != null)
+			{
+				// prendo il carrello dalla sessione 
+				var carrelloSession = HttpContext.Session.GetString("carrello");
+
+				// se il carrello esiste e non è vuoto
+				if (carrelloSession != null)
+				{
+					// deserializzo il carrello 
+					List<Carrello> cart = JsonConvert.DeserializeObject<List<Carrello>>(carrelloSession);
+
+					// controllo i prodotti presenti nel carrello e se un prodotto ha gia id uguale a quello in entrata come parametro
+					// incremento la quantità di 1
+
+					foreach (var item in cart)
+					{
+						if (item.Prodotto.IdProdotto == idProdotto)
+						{
+							item.Quantita++;
+							prodottoGiaPresenteNelCarrello = true;
+
+							// serializzo di nuovo il carrello e lo salvo in sessione
+							HttpContext.Session.SetString("carrello", JsonConvert.SerializeObject(cart));
+							TempData["Message"] = "Hai aggiunto una quantità in più";
+							return RedirectToAction("Index", "Prodotti");
+						}
+					}
+				}
+
+				// se il prodotto non è presente nel carrello
+				if (!prodottoGiaPresenteNelCarrello)
+				{
+					Carrello cart = new Carrello
+					{
+						Prodotto = new Prodotti
+						{
+							IdProdotto = prodottoSelezionato.IdProdotto,
+							Nomeprodotto = prodottoSelezionato.Nomeprodotto,
+							FotoProdotto = prodottoSelezionato.FotoProdotto,
+							Prezzo = prodottoSelezionato.Prezzo,
+							PossibiliUsi = prodottoSelezionato.PossibiliUsi
+
+						},
+						Quantita = 1
+					};
+
+
+
+					//prendiamo il carrello dalla sessione 
+					var carrelloSessione = HttpContext.Session.GetString("carrello");
+
+					List<Carrello> carrello = new List<Carrello>();
+
+					//se il carrello esiste e non è vuoto lo deserializzo
+					if (!string.IsNullOrEmpty(carrelloSessione))
+					{
+						carrello = JsonConvert.DeserializeObject<List<Carrello>>(carrelloSessione);
+					}
+
+					//aggiungiamo il prodotto al carrello
+					carrello.Add(cart);
+
+					//serializziamo il carrello e lo salviamo in sessione
+					HttpContext.Session.SetString("carrello", JsonConvert.SerializeObject(carrello));
+
+
+					TempData["Message"] = "Hai aggiunto il prodotto al carrello";
+					return RedirectToAction("Index", "Prodotti");
+				}
+
+			}
+
+			TempData["Errore"] = "Il prodotto non esiste.Riprova";
+			return RedirectToAction("Index", "Prodotti");
+		}
+	}
 }
