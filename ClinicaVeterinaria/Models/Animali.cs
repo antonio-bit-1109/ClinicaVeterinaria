acd1f7885578011d9e3696d013ab1099e44b7697
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-03-22 12:27:11 +0100

    giggi

--- ClinicaVeterinaria/Models/Animali.cs
+++ ClinicaVeterinaria/Models/Animali.cs
@@ -1,35 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ClinicaVeterinaria.Models;
+﻿namespace ClinicaVeterinaria.Models;
 
 public partial class Animali
 {
     public int IdAnimale { get; set; }
 
-    public DateTime Dataregistrazione { get; set; }
+	public DateTime Dataregistrazione { get; set; }
 
-    public string NomeAnimale { get; set; } = null!;
+	public string NomeAnimale { get; set; } = null!;
 
-    public string Tipologia { get; set; } = null!;
+	public string Tipologia { get; set; } = null!;
 
-    public string ColoreMantello { get; set; } = null!;
+	public string ColoreMantello { get; set; } = null!;
 
-    public DateTime? Datanascita { get; set; }
+	public DateTime? Datanascita { get; set; }
 
-    public bool HasMicrochip { get; set; }
+	public bool HasMicrochip { get; set; }
 
-    public string? NumMicrochip { get; set; }
+	public string? NumMicrochip { get; set; }
 
-    public string? FotoAnimale { get; set; }
+	public string? FotoAnimale { get; set; }
 
-    public bool HasProprietario { get; set; }
+	public bool HasProprietario { get; set; }
 
-    public int? IdUtente { get; set; }
+	public int? IdUtente { get; set; }
 
-    public virtual Utenti? IdUtenteNavigation { get; set; }
+	public virtual Utenti? IdUtenteNavigation { get; set; }
 
-    public virtual ICollection<Ricoveri> Ricoveris { get; set; } = new List<Ricoveri>();
+	public virtual ICollection<Ricoveri> Ricoveris { get; set; } = new List<Ricoveri>();
 
-    public virtual ICollection<Visite> Visites { get; set; } = new List<Visite>();
+	public virtual ICollection<Visite> Visites { get; set; } = new List<Visite>();
 }
