6766c91684cdcfc9e4f41209efa6257fe789bcd7
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-03-22 15:23:14 +0100

    ggg

--- ClinicaVeterinaria/Models/Animali.cs
+++ ClinicaVeterinaria/Models/Animali.cs
@@ -1,51 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
-namespace ClinicaVeterinaria.Models
+namespace ClinicaVeterinaria.Models;
+
+public partial class Animali
 {
-    public partial class Animali
-    {
-        public int IdAnimale { get; set; }
+    public int IdAnimale { get; set; }
 
-        [Display(Name = "Data di Registrazione")]
-        public DateTime Dataregistrazione { get; set; }
+    public DateTime Dataregistrazione { get; set; }
 
-        [Display(Name = "Nome dell'Animale")]
-        [Required(ErrorMessage = "Il nome dell'animale è obbligatorio.")]
-        public string NomeAnimale { get; set; } = null!;
+    public string NomeAnimale { get; set; } = null!;
 
-        [Display(Name = "Tipologia")]
-        [Required(ErrorMessage = "La tipologia dell'animale è obbligatoria.")]
-        public string Tipologia { get; set; } = null!;
+    public string Tipologia { get; set; } = null!;
 
-        [Display(Name = "Colore del Mantello")]
-        [Required(ErrorMessage = "Il colore del mantello è obbligatorio.")]
-        public string ColoreMantello { get; set; } = null!;
+    public string ColoreMantello { get; set; } = null!;
 
-        [Display(Name = "Data di Nascita")]
-        [DataType(DataType.Date)]
-        public DateTime? Datanascita { get; set; }
+    public DateTime? Datanascita { get; set; }
 
-        [Display(Name = "Microchip presente?")]
-        public bool HasMicrochip { get; set; }
+    public bool HasMicrochip { get; set; }
 
-        [Display(Name = "Numero Microchip")]
-        public string? NumMicrochip { get; set; }
+    public string? NumMicrochip { get; set; }
 
-        [Display(Name = "Foto dell'Animale")]
-        public string? FotoAnimale { get; set; }
+    public string? FotoAnimale { get; set; }
 
-        [Display(Name = "Proprietario presente?")]
-        public bool HasProprietario { get; set; }
+    public bool HasProprietario { get; set; }
 
-        public int? IdUtente { get; set; }
+    public int? IdUtente { get; set; }
 
-        [Display(Name = "Utente")]
-        public virtual Utenti? IdUtenteNavigation { get; set; }
+    public virtual Utenti? IdUtenteNavigation { get; set; }
 
-        public virtual ICollection<Ricoveri> Ricoveris { get; set; } = new List<Ricoveri>();
+    public virtual ICollection<Ricoveri> Ricoveris { get; set; } = new List<Ricoveri>();
 
-        public virtual ICollection<Visite> Visites { get; set; } = new List<Visite>();
-    }
+    public virtual ICollection<Visite> Visites { get; set; } = new List<Visite>();
 }
--- ClinicaVeterinaria/Models/ProdottiInCassetto.cs
+++ ClinicaVeterinaria/Models/ProdottiInCassetto.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace ClinicaVeterinaria.Models;
 
@@ -12,8 +11,6 @@ public partial class ProdottiInCassetto
 
     public int? IdCassetto { get; set; }
 
-    [Display(Name = "Quantità")]
-    [Range(0, int.MaxValue, ErrorMessage = "La quantità deve essere maggiore o uguale a zero.")]
     public int Quantita { get; set; }
 
     public virtual Cassetti? IdCassettoNavigation { get; set; }
